eca5ca902cfd247ad06e87e11c60a5d8b404bfb8
Polarcraft <tripps41@hotmail.com>
2015-04-11 05:00:13 -0500

    Update Main.java

--- src/admingui/yougold/org/Main.java
+++ src/admingui/yougold/org/Main.java
@@ -21,6 +21,7 @@ import admingui.yougold.org.events.MenuClickListener;
 public class Main extends JavaPlugin {
 
 	public void onEnable() {
+		//Implements Listeners and Commands
 		getServer().getPluginManager().registerEvents(new MenuClickListener(), this);
 		getCommand("feed").setExecutor(new Feed());
 		getCommand("heal").setExecutor(new Heal());
